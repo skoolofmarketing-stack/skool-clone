1a50a352ba81ed6316072d7312e3098b25a7d127
skoolofmarketing-stack <skoolofmarketing@gmail.com>
2026-03-23 21:39:49 +1100

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -5,6 +5,10 @@ import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Community from './pages/Community';
+import Courses from './pages/Courses';
+import Profile from './pages/Profile';
+import Leaderboard from './pages/Leaderboard';
 
 const queryClient = new QueryClient();
 
@@ -19,9 +23,13 @@ export default function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/"         element={<Home />} />
-          <Route path="/login"    element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/"            element={<Home />} />
+          <Route path="/login"       element={<Login />} />
+          <Route path="/register"    element={<Register />} />
+          <Route path="/community/:slug" element={<Community />} />
+          <Route path="/courses"     element={<Courses />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/profile"     element={<PrivateRoute><Profile /></PrivateRoute>} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
